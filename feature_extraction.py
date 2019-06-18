34b7788f0ae7cee7357b7124388dd1ea92cd5b60
hoang6k <daohoang.hust@gmail.com>
2019-06-18 09:28:01 +0700

    done v0.1 + upload model

---
+++ feature_extraction.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+index_glob = 0
+
+def convolution(img, kernel):
+    h = img.shape[0]
+    w = img.shape[1]
+    img_w_pad = np.zeros((h + 2, w + 2))
+    img_w_pad[0, 1:-1] = img[0]
+    img_w_pad[-1, 1:-1] = img[-1]
+    img_w_pad[1:-1, 0] = img[:, 0]
+    img_w_pad[1:-1, -1] = img[:, -1]
+    img_w_pad[1:-1, 1:-1] = img
+
+    grad = np.zeros_like(img)
+    for i in range(h):
+        for j in range(w):
+            grad[i, j] = np.sum(kernel * img_w_pad[i:i + 3, j:j + 3])
+    return grad
+
+
+def grad_extract(img, kernel):
+    gx = convolution(img, kernel)
+    gy = convolution(img, kernel.T)
+    grad_mag = np.sqrt(gx ** 2 + gy ** 2)
+    grad_angle = np.arctan2(gy, gx)
+    grad_angle = np.rad2deg(np.where(grad_angle < 0, grad_angle + np.pi, grad_angle))
+    return grad_mag, grad_angle
+
+
+def block_extract(grad_mag, grad_angle):
+    feature = np.zeros(9)
+    for i in range(grad_mag.shape[0] ** 2):
+        bin_ratio, bin_idx = np.modf((grad_angle.item(i) + 10) / 20)
+        bin_idx = int(bin_idx % 9)
+        feature[bin_idx - 1] += (1 - bin_ratio) * grad_mag.item(i)
+        feature[bin_idx] += bin_ratio * grad_mag.item(i)
+    return feature
+
+
+def block_feature(grad_mag, grad_angle):
+    h = grad_mag.shape[0]
+    w = grad_mag.shape[1]
+    block_histogram = np.zeros((h // 8, w // 8, 9))
+    feature_block = np.zeros((h // 8 - 1, w // 8 - 1, 36))
+    for i in range(h//8):
+        for j in range(w//8):
+            block_histogram[i, j] = block_extract(grad_mag[8*i:8*i + 8, 8*j:8*j + 8], grad_angle[8*i:8*i + 8, 8*j:8*j + 8])
+    for i in range(h // 8 - 1):
+        for j in range(w // 8 - 1):
+            feature_block[i, j] = np.linalg.norm(np.concatenate(block_histogram[i:i + 2, j:j + 2], axis=None))
+    return np.concatenate(feature_block, axis=None)
+
+
+def extract(img):
+    global index_glob
+    print("Xu ly den anh " + str(index_glob))
+    index_glob += 1
+    k = img.shape[0]
+    x_kernel = np.array([[0., 0., 0.],
+                         [-1., 0, 1.],
+                         [0., 0., 0.]])
+    feature = []
+    for c in range(k):
+        c_img = np.float32(img[c]) / 255.0
+        grad_mag, grad_angle = grad_extract(c_img, x_kernel)
+        feature.append(block_feature(grad_mag, grad_angle))
+    return np.concatenate(feature)
+
+
+def feature_extraction(imgs):
+    data = [extract(img) for img in imgs]
+    return np.asarray(data)
+
+
+if __name__ == "__main__":
+    # train_pos = np.load('train_pos_rgb.npy')
+    # train_neg = np.load('train_neg_rgb.npy')
+    # train_pos = np.load('train_pos.npy')
+    # train_neg = np.load('train_neg.npy')
+
+    # data_pos = feature_extraction(train_pos)
+    # np.save('feature_pos.npy', data_pos)
+    data_pos = np.load('feature_pos.npy')
+
+    # data_neg = feature_extraction(train_neg)
+    # np.save('feature_neg.npy', data_neg)
+    data_neg = np.load('feature_neg.npy')
---
+++ read_data.py
@@ -0,0 +1,60 @@
+import numpy as np
+import cv2
+import glob
+
+
+def load_pos(path, padding):
+    items = glob.glob(path + '*.*')
+    print(len(items))
+    data = [cv2.imread(path, cv2.IMREAD_GRAYSCALE)[padding:-padding, padding:-padding].reshape(1, 1, 128, 64) for path in items]
+    return np.concatenate(data)
+
+
+def random_subimg(img):
+    height = img.shape[0]
+    width = img.shape[1]
+    max_height = height - 128
+    max_width = width - 64
+    sub_imgs = []
+    for i in range(3):
+        np.random.seed(i)
+        vtc = np.random.randint(0, max_height)
+        hzt = np.random.randint(0, max_width)
+        sub_imgs.append(img[vtc:vtc + 128, hzt:hzt + 64].reshape((1, 1, 128, 64)))
+    return np.concatenate(sub_imgs)
+
+
+def load_neg(path):
+    items = glob.glob(path + '*')
+    print(len(items))
+    imgs = []
+    for path in items:
+        imgs.append(random_subimg(cv2.imread(path, cv2.IMREAD_GRAYSCALE)))
+    print(np.concatenate(imgs).shape)
+    return np.concatenate(imgs)
+
+
+if __name__ == "__main__":
+    # train_pos_path = 'INRIAPerson/96X160H96/Train/pos/'
+    # train_pos = load_pos(train_pos_path, 16)
+    # np.save('train_pos.npy', train_pos)
+    # np.save('train_pos_rgb.npy', train_pos)
+    # train_pos = np.load('train_pos_rgb.npy')
+    # train_pos = np.load('train_pos.npy')
+
+    # train_neg_path = 'INRIAPerson/Train/neg/'
+    # train_neg = load_neg(train_neg_path)
+    # np.save('train_neg.npy', train_neg)
+    # np.save('train_neg_rgb.npy', train_neg)
+    # train_neg = np.load('train_neg.npy')
+
+
+    # test_pos_path = 'INRIAPerson/70X134H96/Test/pos/'
+    # test_pos = load_pos(test_pos_path, 3)
+    # np.save('test_pos.npy', test_pos)
+    test_pos = np.load('test_pos.npy')
+
+    # test_neg_path = 'INRIAPerson/Test/neg/'
+    # test_neg = load_neg(test_neg_path)
+    # np.save('test_neg.npy', test_neg)
+    test_neg = np.load('test_neg.npy')
